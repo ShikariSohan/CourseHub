bdc4fe4d8bc626ea47b8c5381679fd68dda2c213
nowshin alam owishi <owishibooo@pop-os.localdomain>
2023-04-01 16:42:33 +0600

    added user

---
+++ src/main/java/sohan/mongodbtutorial/converter/UserConverter.java
@@ -0,0 +1,31 @@
+package sohan.mongodbtutorial.converter;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import sohan.mongodbtutorial.model.User;
+
+public class UserConverter {
+
+    // convert Product Object to MongoDB Document
+    // take special note of converting id String to ObjectId
+    public static Document toDocument(User u) {
+        Document doc = new Document("name", u.getName());
+        doc.append("password", u.getPassword());
+        doc.append("role", u.getRole());
+        doc.append("email", u.getEmail());
+        if (u.getId() != null) {
+            doc.append("_id", new ObjectId(u.getId()));
+        }
+        return doc;
+    }
+
+    public static User toUser(Document doc) {
+        User u = new User();
+        u.setName((String) doc.get("name"));
+        u.setEmail((String) doc.get("email"));
+        u.setPassword((String) doc.get("password"));
+        u.setRole((String) doc.get("role"));
+        u.setId(((ObjectId) doc.get("_id")).toString());
+        return u;
+    }
+}
